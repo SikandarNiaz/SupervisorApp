9c2bc7f60dc8f67d4c65b712a5b2a0c484630a82
Sikandar Niaz <sikandar.niaz@marksman.com.pk>
2020-08-05 11:57:49 +0500

    Initila Commit

---
+++ src/assets/config.ts
@@ -0,0 +1,12 @@
+import { environment } from 'src/environments/environment';
+
+export const config = {
+   ip: 'https://haleeb.rtdtradetracker.com/',
+    // ip: 'http://localhost:8080/audit/',
+  // ip: 'http://192.168.0.101:8080/audit/',
+  hash: environment.hash,
+  main_logo: 'assets/images/logo.png',
+  login_theme_color: 'green',
+  login_logo: 'assets/images/logoSmall.png'
+
+};
