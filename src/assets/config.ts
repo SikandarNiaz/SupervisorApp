91d0ab02576dfef15fc0d8c3e411580208f20768
Sikandar Niaz <sikandar.niaz@marksman.com.pk>
2020-10-29 18:37:11 +0500

    config changes

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,13 +1,13 @@
 import { environment } from "src/environments/environment";
 
 export const config = {
-  // ip: "http://pngjba.rtdtradetracker.com/",
-  //  ip: 'http://groomingba.rtdtradetracker.com/',
+  ip: "http://pngjba.rtdtradetracker.com/",
+  // ip: "http://groomingba.rtdtradetracker.com/",
   // ip: "https://nflce.rtdtradetracker.com/",
-  // ip: "http://localhost:8080/CE/",
+  //  ip: "http://localhost:8080/CE/",
   // ip: 'http://192.168.0.101:8080/audit/',
   // ip: 'http://test1.concavetech.com/',
-  ip: "http://pmicensus.concavetech.com/",
+  // ip: "http://pmicensus.concavetech.com/",
   hash: environment.hash,
   main_logo: "assets/images/logo.png",
   login_theme_color: "green",
