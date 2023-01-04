9fcbd405d47114c6a2471d836ea6169621c9c71f
DESKTOP-1QNK9O9\ATOnline <bilal.ahmad.riaz75@gmail.com>
2023-01-04 11:17:38 +0500

    main page change querry merge

--- src/app/layout/dashboard/innerComponents/interception-summary/interception-summary.component.ts
+++ src/app/layout/dashboard/innerComponents/interception-summary/interception-summary.component.ts
@@ -17,6 +17,8 @@ import { environment } from "src/environments/environment";
   styleUrls: ["./interception-summary.component.scss"],
 })
 export class InterceptionSummaryComponent implements OnInit {
+  dashboardStatsObj: any={};
+  filteredList: any;
   constructor(
     private toastr: ToastrService,
     private httpService: DashboardService,
@@ -31,6 +33,7 @@ export class InterceptionSummaryComponent implements OnInit {
   loading: boolean;
   selectedZone: any = {};
   selectedRegion: any = {};
+  successful: any = {};
   minDate = new Date(2000, 0, 1);
   maxDate = new Date();
   startDate = new Date();
@@ -158,32 +161,79 @@ export class InterceptionSummaryComponent implements OnInit {
     };
 
     this.httpService.getBAList(obj).subscribe((data: any) => {
-      // console.log('merchandiser list for evaluation',data);
+       console.log('merchandiser list for evaluation',data);
       if (data) {
         this.tableData = data;
+        this.getDashboardStats();
       }
       this.loading = false;
+      this.loadingData=false;
     });
   }
+  
+  getDashboardStats() {
+    this.dashboardStatsObj.id = this.tableData.length
+
+    this.dashboardStatsObj.TotalInterception = this.tableData
+      .map((a) => a.TotalInterception)
+      .reduce((a, b) => a + b, 0);
+    this.dashboardStatsObj.Impressions = this.tableData
+      .map((a) => a.Impressions)
+      .reduce((a, b) => a + b, 0);
+    this.dashboardStatsObj.Successful = this.tableData
+      .map((a) => a.Successful)
+      .reduce((a, b) => a + b, 0);
+      this.dashboardStatsObj.Successful_per = (
+        (this.dashboardStatsObj.Successful * 100) /
+        this.dashboardStatsObj.Impressions
+      ).toFixed();
+      
+      this.dashboardStatsObj.Successful=this.dashboardStatsObj.Successful+"("+this.dashboardStatsObj.Successful_per+"%)";
+
+    this.dashboardStatsObj.successful_per_day = this.tableData
+      .map((a) => a.successful_per_day)
+      .reduce((a, b) => a + b, 0);
+
+    this.dashboardStatsObj.TotalSale = this.tableData
+      .map((a) => a.TotalSale)
+      .reduce((a, b) => a + b, 0);
+
+      this.dashboardStatsObj.unit_sale_per_day = this.tableData
+      .map((a) => a.unit_sale_per_day)
+      .reduce((a, b) => a + b, 0);
+
+  //   if (this.projectType == "Coke_Audit") {
+  //  this.setChartData();
+  //   }
+  }
+  
   getDashboardData() {
     this.loadingData = true;
     const obj = {
       startDate: moment(this.startDate).format("YYYY-MM-DD"),
       endDate: moment(this.endDate).format("YYYY-MM-DD"),
-      zoneId: this.selectedZone.id || -1,
-      regionId: this.selectedRegion.id || -1,
+      zoneId:this.selectedZone.id
+      ? this.selectedZone.id == -1
+        ? localStorage.getItem("zoneId")
+        : this.selectedZone.id
+      : localStorage.getItem("zoneId"),
+      regionId:this.selectedRegion.id
+      ? this.selectedRegion.id == -1
+        ? localStorage.getItem("regionId")
+        : this.selectedRegion.id
+      : localStorage.getItem("regionId"),
       shopId: this.selectedStore.id || -1,
       brandId: this.selectedBrand.id || -1,
     };
     this.loading = true;
     this.getTabsData();
-    this.httpService.getDashboardStats(obj).subscribe((data: any) => {
-      this.loadingData = false;
-      if (data) {
-        this.dashboardData = data;
-      }
-      this.loading = false;
-    });
+    // this.httpService.getDashboardStats(obj).subscribe((data: any) => {
+    //   this.loadingData = false;
+    //   if (data) {
+    //     this.dashboardData = data;
+    //   }
+    //   this.loading = false;
+    // });
   }
 
   getArrowType(key) {
