82e6e5e6724ab990509f4699c3271363914ade74
DESKTOP-1QNK9O9\ATOnline <bilal.ahmad.riaz75@gmail.com>
2024-04-19 12:29:10 +0500

    config file

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -2,22 +2,32 @@ import { environment } from "src/environments/environment";
 
 export class Config {
   // ip: "https://png.rtdtradetracker.com/",
+  // ip: "http://localhost:8080/audit/";
   //  ip: 'http://pg.concavetech.com/',
   // ip: "http://pghanger.concavetech.com/",
-  // public static BASE_URI = "https://pgrdt.concavetech.com/";
-
-  // public static BASE_URI = window.location.origin + "/";
-
-  public static BASE_URI = "http://localhost:8095/CE/";
-
-  // for nestle_survey live
-  // public static BASE_URI = "http://nestlesurvey.concavetech.com/";
+   public static BASE_URI = window.location.origin + "/";
+ // public static BASE_URI = "http://pgrdt.concavetech.com/";
+  // public static BASE_URI = "http://pguae.concavetech.com/";
+ // public static BASE_URI = "http://pngiba.concavetech.com/";
+ // public static BASE_URI = "http://tapalsurvey.concavetech.com/";
+ //  public static BASE_URI = "http://rural.concavetech.com/";
+  // public static BASE_URI = "http://localhost:8080/CE/";
+  // public static BASE_URI = "http://daladansm.concavetech.com/";
+//  public static BASE_URI = "http://groomingba.rtdtradetracker.com//";
+  // public static BASE_URI = "http://nflce.concavetech.com/";
+  // public static BASE_URI = "http://samsungba.concavetech.com//";
+ // public static BASE_URI = "http://daldaba.concavetech.com//";
+//  public static BASE_URI = "http://pilotba.concavetech.com//";
+//public static BASE_URI = "https://pmitraining.rtdtradetracker.com//";
+// public static BASE_URI = "http://ndnba.concavetech.com//";
+  // public static BASE_URI = "http://reckittcensus.concavetech.com//";
 
   public static hash = environment.hash;
   public static main_logo = "assets/images/logo.png";
   public static login_theme_color = "green";
   public static login_logo = "assets/images/logoSmall.png";
   public static no_image = "assets/images/noimage.jpg";
-
-  public static API_KEY = '';
+  public static API_KEY = "AIzaSyDg9N44ZNw6gWSfxbsXqw5ONyEiWfL_jEY";
+  // public static MAPBOX_TOKEN =
+  //   "pk.eyJ1IjoiaGFsZWViY29uY2F2ZXRlY2hjb20iLCJhIjoiY2xpbjF0cnA3MHFjMDNrcGNidHdyZngzZiJ9.8J-Ge14qfoHmbkzIHL5ofg";
 }
