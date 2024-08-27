f3c76b58447f4f0ae97ee55d46ba5a983103b92e
DESKTOP-1QNK9O9\ATOnline <bilal.ahmad.riaz75@gmail.com>
2024-08-28 04:48:19 +0500

    config.ts

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -5,25 +5,26 @@ export class Config {
   // ip: "http://localhost:8080/audit/";
   //  ip: 'http://pg.concavetech.com/',
   // ip: "http://pghanger.concavetech.com/",
-  //  public static BASE_URI = window.location.origin + "/";
+     public static BASE_URI = window.location.origin + "/";
  // public static BASE_URI = "http://pgrdt.concavetech.com/";
   // public static BASE_URI = "http://pguae.concavetech.com/";
  // public static BASE_URI = "http://pngiba.concavetech.com/";
  // public static BASE_URI = "http://tapalsurvey.concavetech.com/";
  //  public static BASE_URI = "http://rural.concavetech.com/";
-  // public static BASE_URI = "http://localhost:8085/CE/";
+  // public static BASE_URI = "http://localhost:8080/CE/";
   // public static BASE_URI = "http://daladansm.concavetech.com/";
 //  public static BASE_URI = "http://groomingba.rtdtradetracker.com//";
   // public static BASE_URI = "http://nflce.concavetech.com/";
   // public static BASE_URI = "http://samsungba.concavetech.com//";
  // public static BASE_URI = "http://daldaba.concavetech.com//";
- public static BASE_URI = "http://pilotba.concavetech.com//";
-//public static BASE_URI = "https://pmitraining.rtdtradetracker.com//";
+//  public static BASE_URI = "http://pilotba.concavetech.com//";
+//  public static BASE_URI = "https://ptcba.rtdtradetracker.com//";
 // public static BASE_URI = "http://ndnba.concavetech.com//";
   // public static BASE_URI = "http://reckittcensus.concavetech.com//";
 
   public static hash = environment.hash;
   public static main_logo = "assets/images/logo.png";
+  public static main_logo_velo = "assets/images/Velo.png";
   public static login_theme_color = "green";
   public static login_logo = "assets/images/logoSmall.png";
   public static no_image = "assets/images/noimage.jpg";
