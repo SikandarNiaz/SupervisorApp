3cc1214e0ee6c75bb77038360e6788e62c67510b
DESKTOP-IGF64NH\adeel <m.adeel.bashir@marksman.com.pk>
2024-09-30 17:04:06 +0500

    Ptc VisitDate and Supervisor Attendence

---
+++ src/app/supervisor-attendence-detail/supervisor-attendence-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supervisor-attendence-detail',
+  templateUrl: './supervisor-attendence-detail.component.html',
+  styleUrls: ['./supervisor-attendence-detail.component.css']
+})
+export class SupervisorAttendenceDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
